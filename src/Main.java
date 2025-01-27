739acda0bcc3f70296467dfc454d97457a594605
christian7974 <ctropeano@uri.edu>
2025-01-27 15:49:04 -0500

    Implemented Stack Class
    Implemented Stack class and required methods.

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,10 @@ public class Main {
   public static void main(String[] args) {
     LinkedList list = new LinkedList(1, 2, 3, 5);
     Stack stack1 = new Stack();
+    stack1.push(1);
+    stack1.push(2);
+    stack1.push(3);
+    stack1.push(4);
+    stack1.printStack();
   }
 }
--- src/Stack_LL/Stack.java
+++ src/Stack_LL/Stack.java
@@ -1,6 +1,9 @@
 package Stack_LL;
+
 import Linked_List.Node;
+
 public class Stack {
+
     int length;
     Node top;
     Node bottom = null;
@@ -11,15 +14,66 @@ public class Stack {
         this.length = 0;
     }
 
+    /**
+     * Method that prints out the Stack, starting at the top
+     */
+    public void printStack() {
+        Node currNode = this.top;
+        while (currNode != null) {
+            if (currNode == this.top) { // currNode is the top of the stack
+                System.out.println(currNode.data + " <- Top of stack");
+            } else if (currNode.next == null) { // currNode is the bottom of the stack
+                System.out.print(currNode.data + "\n");
+                return;
+            } else { //
+                System.out.println(currNode.data);
+
+            }
+            System.out.println("--------");
+            currNode = currNode.next;
+        }
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+
+    /**
+     * Method that returns the Node at the top of the stack
+     *
+     * @return the top of the Stack
+     */
     public Node peek() {
-        return new Node(1);
+        return this.top;
     }
 
-    public Node push(int value) {
-        return new Node(1);
+    /**
+     * Add a new Node to the top of the Stack
+     *
+     * @param value the value to be inserted at the top of the stack
+     */
+    public void push(int value) {
+        Node newNode = new Node(value); // create new node
+        if (length == 0) { // if the length is 0 (empty stack), set top and bottom to new node
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            Node temp = this.top; // set a temp node to the top
+            this.top = newNode; // the top is the new node
+            this.top.next = temp; // connect the top to the temp node (connect the stack)
+        }
+        length++;
     }
 
+    /**
+     * Remove the top of the Stack
+     */
     public void pop() {
+        if (this.length == 0) return;
+        if (this.top == this.bottom) this.bottom = null;
+        // set the top to the node that is below the top (the second from the top is the new top)
+        this.top = this.top.next;
+        length--;
 
     }
 }
